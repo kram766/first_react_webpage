6eb402af3f3330cfb1addaee80e576798e9c6a7b
kram766 <36835611+kram766@users.noreply.github.com>
2020-07-18 17:21:25 +0530

    Updates

---
+++ precache-manifest.e15f9be9de84b79fbdd20369ede29334.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fceb73eda3323e75043ee1089c441404",
+    "url": "/first_react_webpage/index.html"
+  },
+  {
+    "revision": "074e15bd4c5c3dedfc6b",
+    "url": "/first_react_webpage/static/css/2.795f4d00.chunk.css"
+  },
+  {
+    "revision": "4858274e2e9906005529",
+    "url": "/first_react_webpage/static/css/main.ebbcf009.chunk.css"
+  },
+  {
+    "revision": "074e15bd4c5c3dedfc6b",
+    "url": "/first_react_webpage/static/js/2.4cd0a24d.chunk.js"
+  },
+  {
+    "revision": "51ccd56507b155fc340d16b580bc1a5d",
+    "url": "/first_react_webpage/static/js/2.4cd0a24d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4858274e2e9906005529",
+    "url": "/first_react_webpage/static/js/main.b7e8590b.chunk.js"
+  },
+  {
+    "revision": "447048e8d2ffbfe5b9ae",
+    "url": "/first_react_webpage/static/js/runtime-main.494503f2.js"
+  },
+  {
+    "revision": "706eb685d2008a74338b9f9926bf122f",
+    "url": "/first_react_webpage/static/media/aliceblue.706eb685.svg"
+  },
+  {
+    "revision": "8e4e660992d2efa53c6aa3f8f400ff12",
+    "url": "/first_react_webpage/static/media/axis.8e4e6609.svg"
+  },
+  {
+    "revision": "d6bac6bad67976459c6a0ff26fb3f0da",
+    "url": "/first_react_webpage/static/media/city.d6bac6ba.svg"
+  },
+  {
+    "revision": "c5122f598432c6ae7fa7f64f0450f45e",
+    "url": "/first_react_webpage/static/media/control.c5122f59.svg"
+  },
+  {
+    "revision": "907c36faa9a7df24a919c74aba2bd92f",
+    "url": "/first_react_webpage/static/media/couple.907c36fa.svg"
+  },
+  {
+    "revision": "650cdccf71611d8ca8d9a870e42ff0b9",
+    "url": "/first_react_webpage/static/media/creator-scroll.650cdccf.mp4"
+  },
+  {
+    "revision": "fc0d314329ac1a5dce0b88089920243b",
+    "url": "/first_react_webpage/static/media/edelweiss.fc0d3143.svg"
+  },
+  {
+    "revision": "eded6a96711ff02ac0c98385b7b64098",
+    "url": "/first_react_webpage/static/media/fivepaisa.eded6a96.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/first_react_webpage/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/first_react_webpage/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/first_react_webpage/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/first_react_webpage/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/first_react_webpage/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "22372a987a51387ff47f39c391b5f27d",
+    "url": "/first_react_webpage/static/media/hdfc.22372a98.svg"
+  },
+  {
+    "revision": "5e2268019db29466d9491ea38c0e0d05",
+    "url": "/first_react_webpage/static/media/hero.5e226801.svg"
+  },
+  {
+    "revision": "142c9178c4cedbc061ef42e4b610e67e",
+    "url": "/first_react_webpage/static/media/icon-awi.142c9178.png"
+  },
+  {
+    "revision": "bfbafc9c0c2ad6f005a2bdb4c1080997",
+    "url": "/first_react_webpage/static/media/ideas.bfbafc9c.svg"
+  },
+  {
+    "revision": "66ab3a8cab80f7a6bf45b009df02b533",
+    "url": "/first_react_webpage/static/media/iifl.66ab3a8c.svg"
+  },
+  {
+    "revision": "6840505bf7dcbbb47bd954ff13213771",
+    "url": "/first_react_webpage/static/media/kite.6840505b.svg"
+  },
+  {
+    "revision": "420012ffad46b5a167b8d3081f0a062f",
+    "url": "/first_react_webpage/static/media/kotak.420012ff.svg"
+  },
+  {
+    "revision": "bed68f2f32ebe7011c04c4153bf4fda4",
+    "url": "/first_react_webpage/static/media/logo-full.bed68f2f.svg"
+  },
+  {
+    "revision": "248a1a116e7bf274bfc4fcccfbbbc926",
+    "url": "/first_react_webpage/static/media/lower-risk.248a1a11.svg"
+  },
+  {
+    "revision": "68632ab5d03549646762d26f581b2ce8",
+    "url": "/first_react_webpage/static/media/model.68632ab5.svg"
+  },
+  {
+    "revision": "3113ccbfcf0eedc5574deda6fb84cb12",
+    "url": "/first_react_webpage/static/media/zero-fees.3113ccbf.svg"
+  }
+]);
